724bec0d69e505c605de281a5eb13c2fe58fc03d
LuisaFernandas1017 <44326462+LuisaFernandas1017@users.noreply.github.com>
2020-03-12 11:03:17 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main{
+	
+	public static void main(String[] args) {
+		
+	}
+	
+
+
+	public void mensajeRama5(){
+	 System.out.printl("Este es el mensaje de la rama5");
+
+	public void mensajerama4(){
+	 System.out.printl("Este es el mensaje de la rama4");
+
+	public void mensajerama3(){
+	 System.out.println("Este es el mensaje de la rama3");
+
+	public void mensajerama2(){
+	System.out.println("Este mensaje es de la rama 2");
+
+
+	}
+
+}
